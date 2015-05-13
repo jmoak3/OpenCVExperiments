154f5ec0f2da364f34ddf6cea8e09366c5b981ff
James Moak <mrjmoak3@gmail.com>
2015-05-13 02:35:39 -0400

    Blue and Green are isolated, performance boosts, Camera speed increases

--- OpenCVPractice.c
+++ OpenCVPractice.c
@@ -3,6 +3,15 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+#include <libv4l2.h>
+#include <linux/videodev2.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+//To Do List: 
+//Read Mesh and draw into frame
+//Track two points and keep track of object using them
+
 struct readCameraThreadObj
 {
 	CvCapture *cam;
@@ -20,6 +29,34 @@ struct getImageObj
 	pthread_mutex_t * mutex;
 	int* frames;
 };
+
+void initializeCamSettings()
+{
+	int descriptor = open("/dev/video0", O_RDWR);
+	
+	struct v4l2_control c;
+	c.id = V4L2_CID_AUTO_WHITE_BALANCE;
+	c.value = 0;
+	ioctl(descriptor, VIDIOC_S_CTRL, &c);	
+
+	c.id = V4L2_CID_EXPOSURE_AUTO_PRIORITY;
+	c.value = 0;
+	ioctl(descriptor, VIDIOC_S_CTRL, &c);
+}
+
+void resetCamSettings()
+{
+	int descriptor = open("/dev/video0", O_RDWR);
+	
+	struct v4l2_control c;
+	c.id = V4L2_CID_AUTO_WHITE_BALANCE;
+	c.value = 1;
+	ioctl(descriptor, VIDIOC_S_CTRL, &c);	
+	
+	c.id = V4L2_CID_EXPOSURE_AUTO_PRIORITY;
+	c.value = 1;
+	ioctl(descriptor, VIDIOC_S_CTRL, &c);
+}
 
 void *readCamera(void *voidParams)
 {
@@ -56,8 +93,20 @@ IplImage *isolateGreen(IplImage * inputImg)
 	IplImage *convertedImg = cvCreateImage(cvGetSize(inputImg), 8, 3);
 	cvCvtColor(inputImg, convertedImg, CV_BGR2HSV);
 	IplImage * img = cvCreateImage(cvGetSize(inputImg), 8, 1);
-	CvScalar lower; lower.val[0] = 30; lower.val[1] = 80; lower.val[2] = 80; lower.val[3] = 0;
-	CvScalar higher; higher.val[0] = 46; higher.val[1] = 255; higher.val[2] = 255; higher.val[3] = 0;
+	CvScalar lower; lower.val[0] = 32.5; lower.val[1] = 0; lower.val[2] = 60; lower.val[3] = 0;
+	CvScalar higher; higher.val[0] = 82.5; higher.val[1] = 255; higher.val[2] = 255; higher.val[3] = 0;
+	cvInRangeS(convertedImg, lower, higher, img);
+	cvReleaseImage(&convertedImg);
+	return img;
+}
+
+IplImage *isolateBlue(IplImage * inputImg)
+{
+	IplImage *convertedImg = cvCreateImage(cvGetSize(inputImg), 8, 3);
+	cvCvtColor(inputImg, convertedImg, CV_BGR2HSV);
+	IplImage * img = cvCreateImage(cvGetSize(inputImg), 8, 1);
+	CvScalar lower; lower.val[0] = 90; lower.val[1] = 0; lower.val[2] = 60; lower.val[3] = 0;
+	CvScalar higher; higher.val[0] = 130; higher.val[1] = 255; higher.val[2] = 255; higher.val[3] = 0;
 	cvInRangeS(convertedImg, lower, higher, img);
 	cvReleaseImage(&convertedImg);
 	return img;
@@ -65,12 +114,13 @@ IplImage *isolateGreen(IplImage * inputImg)
 
 int main(int argc, char** argv )
 {
-	cvNamedWindow("Webcam", 1);
-	cvNamedWindow("GreenIsolated", 1);
-	
+	initializeCamSettings();
+
 	CvCapture *cam = cvCaptureFromCAM(0);
-	//cvSetCaptureProperty(cam, CV_CAP_PROP_AUTO_EXPOSURE, 0);
-	cvSetCaptureProperty(cam, CV_CAP_PROP_EXPOSURE,1);
+	
+	cvNamedWindow("Webcam", 1);
+	cvNamedWindow("GreenAndBlueIsolated", 1);
+	
 	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 	pthread_cond_t can_read = PTHREAD_COND_INITIALIZER;
 	IplImage * image = cvQueryFrame(cam);
@@ -84,7 +134,6 @@ int main(int argc, char** argv )
 	threadParams.can_read = &can_read;
 	threadParams.mutex = &mutex;
 	
-	threadParams.image = cvQueryFrame(threadParams.cam);
 	pthread_create(&readThread, NULL, &readCamera, (void*)&threadParams);
 	
 	int frames = 0;
@@ -94,30 +143,47 @@ int main(int argc, char** argv )
 	float lastTime = timeobj.tv_sec*1000.f + timeobj.tv_nsec/1000000.f;
 	float currTime = lastTime;
 	char key = 0;
+	
+	struct getImageObj imageParams;
+	imageParams.image = image;
+	imageParams.wait = &wait;
+	imageParams.can_read = &can_read;
+	imageParams.mutex = &mutex;
+	imageParams.frames = &frames;
+	IplImage * webCamFrame = getImage(&imageParams);
+	IplImage * greenFrame = isolateGreen(webCamFrame);
+	IplImage * blueFrame = isolateBlue(webCamFrame);
+	
 	while(key!=27) 
 	{	
-		struct getImageObj imageParams;
 		imageParams.image = image;
 		imageParams.wait = &wait;
 		imageParams.can_read = &can_read;
 		imageParams.mutex = &mutex;
 		imageParams.frames = &frames;
-		IplImage * webCamFrame = getImage(&imageParams);
-		IplImage * greenFrame = isolateGreen(webCamFrame);
+		webCamFrame = getImage(&imageParams);
+		greenFrame = isolateGreen(webCamFrame);
+		blueFrame = isolateBlue(webCamFrame);
 		
 		cvShowImage("Webcam", webCamFrame);
-		cvShowImage("GreenIsolated", greenFrame);
+		cvAdd(greenFrame, blueFrame, greenFrame, NULL);
+		cvShowImage("GreenAndBlueIsolated", greenFrame);
 		clock_gettime(CLOCK_MONOTONIC, &timeobj);
 		
 		key = cvWaitKey(1);
+
+		if (key==99)
+			resetCamSettings();
+		if (key==118)
+			initializeCamSettings();
+		
 	}
 	currTime = timeobj.tv_sec*1000.f + timeobj.tv_nsec/1000000.f;
 	totalTime = currTime - lastTime;
 	printf("AVG FPS: %f\n", (1000.f*(float)frames/totalTime));
 	cvReleaseCapture(&cam);
 	cvDestroyWindow("Webcam");	
-	cvDestroyWindow("GreenIsolated");	
-	
+	cvDestroyWindow("GreenAndBlueIsolated");
+		
 	return 0;
 }
-
